65a2e9ed04e946b5caf4d2c0c2c15434025e1933
sophnim <sophnim@gmail.com>
2019-04-21 15:32:39 +0900

    Update dice64.c

--- dice64.c
+++ dice64.c
@@ -1,11 +1,14 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 
 // generates int64_t random : begin <= x <= end 
 int64_t dice64(int64_t begin, int64_t end)
 {
+	srand(time(NULL));
 	uint64_t range = end - begin;
-	if (range == 0 || range < 0) {
+	if (range == 0 || range < 0) { 
+		// if invalid range, return begin
 		return begin;
 	}
 
@@ -14,6 +17,7 @@ int64_t dice64(int64_t begin, int64_t end)
 	uint64_t divSize = 10000;
 	uint64_t sectionSize = 0;
 
+	// select random number between 0 ~ totalSize - 1
 	while (true) {
 		int r = rand(); // 0 ~ RND_MAX(32767)
 		sectionSize = totalSize / divSize;
@@ -25,5 +29,6 @@ int64_t dice64(int64_t begin, int64_t end)
 		}
 	}
 
+	// selected mod range plus begin is randomNumber between begin ~ end 
 	return begin + (selected % range);
 }
